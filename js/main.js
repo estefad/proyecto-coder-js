0b3be9a28cc3129bb712d9c7544c201bbf35f2f3
estefad <stefanadominguez@gmail.com>
2022-05-23 19:51:47 -0300

    primer commit del proyecto

---
+++ js/main.js
@@ -0,0 +1,80 @@
+/*let nombre = "Homero" ;
+let apellido = "Simpson";
+let edad = 38;
+
+
+console.log("mi nombre es: " + nombre +" " + apellido + " " + edad);
+
+const pais1= "francia";
+const pais2= "India";
+const pais3= "España";
+const pais4= "Rusia";
+
+console.log(pais1 + " " + pais2 + " " + pais3 + " " + pais4)
+ 
+
+let fullname= " Bart Simpson";
+let direccion = "742 evergreen terr";
+let ciudad = "springfield, usa";
+let birthday = "02-11-70";
+
+let datos =  fullname + " " + direccion + " " + ciudad + " " + birthday ;
+console.log (datos);
+
+/*let integrante1= prompt("Ingrese integrante 1");
+let integrante2= prompt("Ingrese integrante 2");
+let integrante3= prompt("Ingrese integrante 3");
+let integrante4= prompt("Ingrese integrante 4");
+
+alert (integrante1 + integrante2 +integrante3 +integrante4 );
+
+console.log (integrante1 + " " + integrante2 + " " + integrante3 +  " " + integrante4);
+
+
+let precio = Number( prompt ("Ingrese precio"));
+let descuento= precio * 0.2; 
+let totalDescuento = "Total con Descuento 20%" + (precio - descuento);
+alert(precio);
+alert(totalDescuento);
+console.log (totalDescuento);
+
+
+
+let miNombre = prompt("Ingrese el nombre a comparar: ");
+const elNombre = "Tefa";
+
+if(miNombre === elNombre){
+    alert("Fui Yo!!!!");
+}else{
+    alert("Yo no Fui!!!!");
+} || 
+
+let ingreso= prompt("Ingrese letra");
+if(ingreso== "Y" )
+{
+    alert("Correcto");
+} 
+else {
+    if ( ingreso== "y") {
+        alert("error");
+    } 
+    else { alert("segui intentando");
+    } 
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
